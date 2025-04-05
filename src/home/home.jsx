a858aa17f8e8bc8e175a076dd709f3cb6764fb9d
stivcode21 <stivenelcodificador@gmail.com>
2025-04-05 12:37:27 -0500

    header section basic components configuration

---
+++ src/home/home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/Header";
+import styles from "./home.module.css";
+
+const Home = () => {
+    return (
+        <div id="container"
+            className={styles.container}><Header /></div>
+    )
+}
+
+export default Home
