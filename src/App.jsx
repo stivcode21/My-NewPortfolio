d7755389ca17d829fd21b1bc0857f899abf66d32
stivcode21 <stivenelcodificador@gmail.com>
2025-04-05 12:34:38 -0500

    Routing and logic for theme switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import AppRouter from "./routing/AppRouter";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import "./main.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode === null ? true : savedMode === "true";
+  });
+
+  useEffect(() => {
+    document.body.className = isDarkMode ? "dark-mode" : "light-mode";
+    localStorage.setItem("darkMode", isDarkMode);
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      document.body.className = newMode ? "dark-mode" : "light-mode";
+      localStorage.setItem("darkMode", newMode);
+      return newMode;
+    });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppRouter
+        isDarkMode={isDarkMode}
+        toggleDarkMode={toggleDarkMode}
+      />
+      <SpeedInsights />
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/routing/AppRouter.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../home/home";
+
+export default function AppRouter({ isDarkMode, toggleDarkMode }) {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Home
+                        // isDarkMode={isDarkMode}
+                        // toggleDarkMode={toggleDarkMode}
+                        />
+                    }
+                />
+                <Route
+                    path="/education/:id"
+                    element={<q />}
+                />
+                <Route
+                    path="/project/:url"
+                    element={< q />}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+}
