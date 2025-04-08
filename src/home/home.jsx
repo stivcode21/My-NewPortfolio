4acc0883428d5d8a527a5dda16f986e697a72989
stivcode21 <stivenelcodificador@gmail.com>
2025-04-08 11:09:11 -0500

    new navbar section

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,13 @@
 import Header from "./sections/header/Header";
 import styles from "./Home.module.css";
+import Navbar from "./sections/navbar/Navbar";
 
 const Home = () => {
     return (
         <div id="container"
             className={styles.container}>
+            <Navbar />
+            <Header />
             <Header />
         </div>
     )
---
+++ src/home/sections/navbar/Navbar.jsx
@@ -0,0 +1,199 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import styles from "./Navbar.module.css";
+import { dataBtnIcons } from "../../../data/dataBtnIcons";
+import HoverLabel from "../../../components/hoverLabel/HoverLabel";
+
+
+
+export default function Navbar({ isDarkMode, toggleDarkMode }) {
+    const [listVisible, setListVisible] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const screenWidthCollapsed = 1024;
+
+    const updateCollapsedState = () => {
+        setIsCollapsed(
+            window.innerWidth <= screenWidthCollapsed || window.scrollY > 0
+        );
+    };
+
+    const handleHoverNav = (isHover) => {
+        if (window.innerWidth > screenWidthCollapsed) {
+            setIsCollapsed(!isHover && window.scrollY > 0);
+        }
+    };
+
+    useEffect(() => {
+        updateCollapsedState();
+
+        window.addEventListener("scroll", updateCollapsedState);
+        window.addEventListener("resize", updateCollapsedState);
+
+        return () => {
+            window.removeEventListener("scroll", updateCollapsedState);
+            window.removeEventListener("resize", updateCollapsedState);
+        };
+    }, []);
+
+    const handleScroll = (sectionId) => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            const navbarHeight = document.querySelector("nav").offsetHeight;
+            const offsetTop =
+                section.getBoundingClientRect().top + window.scrollY - navbarHeight;
+
+            window.scrollTo({
+                top: offsetTop,
+                behavior: "smooth",
+            });
+        }
+
+        if (innerWidth <= screenWidthCollapsed) {
+            setListVisible(false);
+        }
+    };
+
+    const getIcon = (title) => {
+        const icon = dataBtnIcons.find((icon) => icon.title === title);
+        return icon ? icon.svg : null;
+    };
+
+    return (
+        <motion.nav
+            className={`${styles.nav} ${listVisible ? styles.open : ""}`}
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.8, ease: [0.215, 0.61, 0.355, 1] }}>
+            <div
+                className={styles.coverList}
+                onClick={() => setListVisible(false)}></div>
+            <div
+                className={`${styles.navLeft} ${isCollapsed ? styles.collapsed : ""}`}
+                onMouseEnter={() => handleHoverNav(true)}
+                onMouseLeave={() => handleHoverNav(false)}
+                onClick={() => setListVisible((prev) => !prev)}>
+                <button
+                    to="#"
+                    className={styles.menu}
+                    aria-label={
+                        !listVisible
+                            ? "Abrir menú de navegación"
+                            : "Cerrar menú de navegación"
+                    }
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div className={styles.links}>
+                    <button
+                        onClick={() => handleScroll("experience")}
+                        className={styles.navLink}>
+                        <span>Inicio</span>
+                    </button>
+                    <button
+                        onClick={() => handleScroll("experience")}
+                        className={styles.navLink}>
+                        <span>Experiencia</span>
+                    </button>
+                    <button
+                        onClick={() => handleScroll("projects")}
+                        className={styles.navLink}>
+                        <span>Proyectos</span>
+                    </button>
+                    <button
+                        onClick={() => handleScroll("education")}
+                        className={styles.navLink}>
+                        <span>Educación</span>
+                    </button>
+                </div>
+            </div>
+            <div className={styles.navRight}>
+                <HoverLabel label={"Idioma"} position="bottom">
+                    <button className={styles.navSetting}>
+                        {getIcon("world")}
+                        <span>EN</span>
+                    </button>
+                </HoverLabel>
+                <HoverLabel label={"Idioma"} position="bottom">
+                    <button
+                        onClick={toggleDarkMode}
+                        className={`${styles.navSetting} ${styles.switchSetting}
+					${isDarkMode ? styles.dark : styles.light}`}>
+                        <div className={styles.backdrop}>
+                            <span className={`${styles.icon} ${styles.sun}`}>
+                                {getIcon("sun")}
+                            </span>
+                            <span className={`${styles.icon} ${styles.moon}`}>
+                                {getIcon("moon")}
+                            </span>
+                        </div>
+                    </button>
+                </HoverLabel>
+            </div>
+            <div className={styles.navList}>
+                <button
+                    className={`${styles.navLink}`}
+                    onClick={() => handleScroll("header")}
+                    style={{
+                        animationDelay: `${0.1}s`,
+                    }}>
+                    <span>Inicio</span>
+                </button>
+                <button
+                    className={styles.navLink}
+                    style={{
+                        animationDelay: `${0.1 * 2}s`,
+                    }}>
+                    <span>Experiencia</span>
+                </button>
+                <button
+                    className={styles.navLink}
+                    style={{
+                        animationDelay: `${0.1 * 3}s`,
+                    }}
+                    onClick={() => handleScroll("projects")}>
+                    <span>Proyectos</span>
+                </button>
+                <button
+                    className={styles.navLink}
+                    style={{
+                        animationDelay: `${0.1 * 4}s`,
+                    }}
+                    onClick={() => handleScroll("education")}>
+                    <span>Educación</span>
+                </button>
+                <div
+                    className={styles.settingList}
+                    style={{
+                        animationDelay: `${0.1 * 5}s`,
+                    }}>
+                    <button
+                        className={styles.navSetting}
+                        aria-label="Cambiar idioma a Inglés (Aún no disponible)">
+                        {getIcon("world")}
+                        <span>EN</span>
+                    </button>
+                    <button
+                        onClick={toggleDarkMode}
+                        className={`${styles.navSetting} ${styles.switchSetting}
+					${isDarkMode ? styles.dark : styles.light}`}
+                        aria-label={isDarkMode ? "Ver modo oscuro" : "Ver modo claro"}>
+                        <div className={styles.backdrop}>
+                            <HoverLabel label={"Idioma"} position="bottom">
+                                <button className={`${styles.icon} ${styles.sun}`}>
+                                    {getIcon("sun")}
+                                </button>
+                            </HoverLabel>
+                            <HoverLabel label={"Idioma"} position="bottom">
+                                <button className={`${styles.icon} ${styles.moon}`}>
+                                    {getIcon("moon")}
+                                </button>
+                            </HoverLabel>
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </motion.nav>
+    );
+}
