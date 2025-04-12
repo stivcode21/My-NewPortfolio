cecebc01ca5a247cc33c4eb078da8b6c87830402
stivcode21 <stivenelcodificador@gmail.com>
2025-04-12 13:04:26 -0500

    new sectionHead

---
+++ src/components/sectionHead/sectionHead.jsx
@@ -0,0 +1,14 @@
+import styles from "./sectionHead.module.css";
+
+export default function SectionHead({ title, description, imageName }) {
+
+	return (
+		<header className={styles.header}>
+			<span className={styles.headerRight}>
+				<h3 className={styles.h3}>{title}
+				</h3>
+				<p className={styles.description}>{description}</p>
+			</span>
+		</header>
+	);
+}
