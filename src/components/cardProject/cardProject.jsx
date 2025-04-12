59c9e5dc7d6aac4ffe818ff756b49f4d5bedd93a
stivcode21 <stivenelcodificador@gmail.com>
2025-04-11 23:39:41 -0500

    new cardProject component

---
+++ src/components/cardProject/cardProject.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "./cardProject.module.css";
+import { Link } from "react-router-dom";
+import Carousel from "../carousel/carousel";
+import Chip from "../chip/chip";
+import LazyImage from "../lazyImage/LazyImage";
+
+export default function CardProject({ project, isMinimal = false }) {
+	return (
+		<Link
+			to={`/project/${project.url}`}
+			state={{ transitionName: `project-image-${project.url}` }}>
+			<article
+				className={`${styles.cardProject} ${isMinimal && styles.minimal}`}>
+				{/* Imagen duplicada con blur al hover */}
+				<div className={styles.blurOverlay}>
+					<LazyImage
+						src={project.images[0].props.src}
+						alt={project.images[0].props.alt}
+						className={styles.blurImage}
+					/>
+				</div>
+				<div className={styles.content}>
+					{isMinimal ? (
+						<div className={styles.cardImage}>
+							<LazyImage
+								src={project.images[0].props.src}
+								alt={project.images[0].props.alt}
+							/>
+						</div>
+					) : (
+						<Carousel
+							carouselImages={project.images}
+							motionId={project.url}
+						/>
+					)}
+
+					<div className={styles.cardFooter}>
+						<span>
+							<span>
+								<h3 className={styles.title}>{project.title}</h3>;
+								{!isMinimal && (
+									<ul>
+										{project.tags.slice(0, 2).map((tag, index) => (
+											<Chip
+												tag={tag}
+												index={index}
+												key={index}
+											/>
+										))}
+									</ul>
+								)}
+							</span>
+							<p>{project.shortDescription}</p>
+						</span>
+					</div>
+				</div>
+			</article>
+		</Link>
+	);
+}
