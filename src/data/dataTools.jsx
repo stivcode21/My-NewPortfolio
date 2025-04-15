9e623721aa3e3adabc265efa28eced5184219b33
stivcode21 <stivenelcodificador@gmail.com>
2025-04-14 23:46:13 -0500

    data tools

---
+++ src/data/dataTools.jsx
@@ -0,0 +1,156 @@
+import FigmaLogo from "../../public/logos/Figma.png";
+import CanvaLogo from "../../public/logos/Canva.png";
+import GoogleDocsLogo from "../../public/logos/GoogleDocs.png";
+import GoogleSlidesLogo from "../../public/logos/GoogleSlides.png";
+import GoogleSheetsLogo from "../../public/logos/GoogleSheets.png";
+import OptimalWorkshopLogo from "../../public/logos/OptimalWorkshop.png";
+
+import ShotsLogo from "../../public/logos/Shots.png";
+
+import HTMLLogo from "../../public/logos/HTML.png";
+import CSSLogo from "../../public/logos/CSS.png";
+import JSLogo from "../../public/logos/JS.png";
+import ReactLogo from "../../public/logos/ReactJS.png";
+import VercelLogo from "../../public/logos/Vercel.png";
+import FirebaseLogo from "../../public/logos/Firebase.png";
+import HostingerLogo from "../../public/logos/Hostinger.png";
+import GithubLogo from "../../public/logos/Github.png";
+
+import PhotoshopLogo from "../../public/logos/Photoshop.png";
+import IllustratorLogo from "../../public/logos/Illustrator.png";
+
+import AsanaLogo from "../../public/logos/Asana.png";
+import ClockifyLogo from "../../public/logos/Clockify.png";
+import NotionLogo from "../../public/logos/Notion.png";
+
+export const dataTools = {
+	// UX/UI TOOLS
+	figma: {
+		title: "Figma",
+		caption: "Diseño, prototipos y workshops",
+		logo: { FigmaLogo },
+		category: 1,
+	},
+	canva: {
+		title: "Canva",
+		caption: "Diseños rápidos y creativos",
+		logo: { CanvaLogo },
+		category: 1,
+	},
+	gdocs: {
+		title: "Google Docs",
+		caption: "Documentos colaborativos",
+		logo: { GoogleDocsLogo },
+		category: 1,
+	},
+	gslides: {
+		title: "Google Slides",
+		caption: "Presentaciones dinámicas",
+		logo: { GoogleSlidesLogo },
+		category: 1,
+	},
+	gsheets: {
+		title: "Google Sheets",
+		caption: "Hojas de cálculo eficientes",
+		logo: { GoogleSheetsLogo },
+		category: 1,
+	},
+	optimalworkshop: {
+		title: "Optimal Workshop",
+		caption: "Pruebas de arquitectura UX",
+		logo: { OptimalWorkshopLogo },
+		category: 1,
+	},
+
+	// UI
+	shots: {
+		title: "Shots",
+		caption: "Mockups personalizados",
+		logo: { ShotsLogo },
+		category: 2,
+	},
+
+	// FRONTEND TOOLS
+	html: {
+		title: "HTML",
+		caption: "Estructura de página web",
+		logo: { HTMLLogo },
+		category: 3,
+	},
+	css: {
+		title: "CSS",
+		caption: "Estilo y diseño web",
+		logo: { CSSLogo },
+		category: 3,
+	},
+	js: {
+		title: "JS",
+		caption: "Desarrollo web interactivo",
+		logo: { JSLogo },
+		category: 3,
+	},
+	reactjs: {
+		title: "React JS",
+		caption: "Webs dinámicas y escalables",
+		logo: { ReactLogo },
+		category: 3,
+	},
+	vercel: {
+		title: "Vercel",
+		caption: "Hosting rápido y sencillo",
+		logo: { VercelLogo },
+		category: 3,
+	},
+	gfirebase: {
+		title: "Firebase",
+		caption: "Hosting con backend integrado",
+		logo: { FirebaseLogo },
+		category: 3,
+	},
+	hostinger: {
+		title: "Hostinger",
+		caption: "Web hosting profesional",
+		logo: { HostingerLogo },
+		category: 3,
+	},
+	github: {
+		title: "Github",
+		caption: "Control de versiones colaborativo",
+		logo: { GithubLogo },
+		category: 3,
+	},
+
+	// GRAPHIC TOOLS
+	photoshop: {
+		title: "Adobe Photoshop",
+		caption: "Edición fotográfica creativa",
+		logo: { PhotoshopLogo },
+		category: 4,
+	},
+	illustrator: {
+		title: "Adobe Illustrator",
+		caption: "Diseño vectorial avanzado",
+		logo: { IllustratorLogo },
+		category: 4,
+	},
+
+	// ORGANIZATION
+	asana: {
+		title: "Asana",
+		caption: "Organización de tareas y deadlines",
+		logo: { AsanaLogo },
+		category: 5,
+	},
+	clockify: {
+		title: "Clockify",
+		caption: "Optimización del tiempo de trabajo",
+		logo: { ClockifyLogo },
+		category: 5,
+	},
+	notion: {
+		title: "Notion",
+		caption: "Gestión y documentación de proyectos",
+		logo: { NotionLogo },
+		category: 5,
+	},
+};
