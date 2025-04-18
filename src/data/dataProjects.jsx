e014baf6eae38ee10cad79c59cb4a638547138ea
stivcode21 <stivenelcodificador@gmail.com>
2025-04-17 22:19:06 -0500

    update data projects

--- src/data/dataProjects.jsx
+++ src/data/dataProjects.jsx
@@ -4,15 +4,10 @@ import Viflow3 from "../assets/projects/viflow3.webp";
 
 
 import UrbanShop1 from "../assets/projects/urbanshop1.webp";
-import Amazon2 from "../assets/projects/Amazon2.jpg";
-import Amazon3 from "../assets/projects/Amazon3.jpg";
-import Amazon4 from "../assets/projects/Amazon4.jpg";
-import Amazon5 from "../assets/projects/Amazon5.jpg";
+import UrbanShop2 from "../assets/projects/urbanshop2.webp";
+import UrbanShop3 from "../assets/projects/urbanshop3.webp";
 
-import Ajaka1 from "../assets/projects/Ajaka1.jpg";
-import Ajaka2 from "../assets/projects/Ajaka2.jpg";
-import Ajaka3 from "../assets/projects/Ajaka3.jpg";
-import Ajaka4 from "../assets/projects/Ajaka4.jpg";
+import FrameOnix1 from "../assets/projects/frameonix1.webp";
 
 import Consultora1 from "../assets/projects/Consultora1.jpg";
 import Consultora2 from "../assets/projects/Consultora2.jpg";
@@ -37,52 +32,32 @@ export const dataProjects = [
     },
     {
         id: 2,
-        title: "Urban Shop",
+        title: "UrbanShop",
         url: "UrbanShop",
         categoryId: 1,
         images: [
             <img src={UrbanShop1} alt="Mockup del proyecto de UrbanShop" />,
-            <img src={Amazon2} alt="Mockup del proyecto de Amazon" />,
-            <img src={Amazon3} alt="Mockup del proyecto de Amazon" />,
-            <img src={Amazon4} alt="Mockup del proyecto de Amazon" />,
-            <img src={Amazon5} alt="Mockup del proyecto de Amazon" />,
+            <img src={UrbanShop2} alt="Mockup del proyecto de UrbanShop" />,
+            <img src={UrbanShop3} alt="Mockup del proyecto de UrbanShop" />,
         ],
-        tags: ["Rediseño", "Web", "E-commerce", "Benchmark"],
+        tags: ["Web", "E-commerce"],
         shortDescription:
             "Una reinvención de la experiencia y diseño visual, eficientizando la búsqueda y compra de productos.",
     },
     {
         id: 3,
-        title: "SkillSwap",
-        url: "skillswap",
-        categoryId: 2,
+        title: "FrameOnix",
+        url: "FrameOnix",
+        categoryId: 1,
         images: [
-            <img src={Ajaka1} alt="Mockup del proyecto de SkillSwap" />,
-            <img src={Ajaka1} alt="Mockup del proyecto de SkillSwap" />,
-            <img src={Ajaka1} alt="Mockup del proyecto de SkillSwap" />,
-            <img src={Ajaka1} alt="Mockup del proyecto de SkillSwap" />,
+            <img src={FrameOnix1} alt="Mockup del proyecto de FrameOnix" />,
         ],
-        tags: ["Creación", "App", "Material Design", "Storytelling"],
+        tags: ["Web", "Library"],
         shortDescription:
             "Busca revolucionar la forma en que aprendemos y compartimos experiencias de aprendizaje.",
     },
     {
         id: 4,
-        title: "Ajaka Envases",
-        url: "ajaka-envases",
-        categoryId: 2,
-        images: [
-            <img src={Ajaka1} alt="Mockup del proyecto de Ajaka Envases" />,
-            <img src={Ajaka2} alt="Mockup del proyecto de Ajaka Envases" />,
-            <img src={Ajaka3} alt="Mockup del proyecto de Ajaka Envases" />,
-            <img src={Ajaka4} alt="Mockup del proyecto de Ajaka Envases" />,
-        ],
-        tags: ["Responsive", "Web", "Landing page", "React Router"],
-        shortDescription:
-            "Landing page adaptable y resolutiva para una empresa de envasado de productos.",
-    },
-    {
-        id: 5,
         title: "Consultora",
         url: "consultora-de-negocios",
         categoryId: 2,
