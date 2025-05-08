f67fd7f8ad9d099ab3bda6584d5660f69ffebcd3
stivcode21 <stivenelcodificador@gmail.com>
2025-05-08 16:44:49 -0500

    new component for comments

---
+++ src/components/commentCard/commentCard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "./commentCard.module.css";
+import { LinkIcon, Pin } from "lucide-react";
+
+const CommentCard = ({
+  avatar,
+  username,
+  comment,
+  date, // ejemplo: "4 Feb 2023"
+  pinned = false,
+  link,
+}) => {
+  return (
+    <div className={styles.guestbook}>
+      {pinned && (
+        <div className={styles.pin}>
+          <Pin size={14} />
+          <span>Pinned Cheep</span>
+        </div>
+      )}
+      <div className={styles.content}>
+        <img src={avatar} alt={username} className={styles.avatar} />
+        <div>
+          <div className={styles.header}>
+            <a
+              className={styles.userName}
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <strong>{username}</strong>
+            </a>
+            <span className={styles.date}>•</span>
+            <span className={styles.date}>{date}</span>
+          </div>
+          <p className={styles.comment}>{comment}</p>
+        </div>
+      </div>
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.linkIcon}
+      >
+        <LinkIcon size={18} />
+      </a>
+    </div>
+  );
+};
+
+export default CommentCard;
