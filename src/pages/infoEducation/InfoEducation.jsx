feab3a71c6806fbfef5fc015af5c735d227db4f8
stivcode21 <stivenelcodificador@gmail.com>
2025-05-17 15:20:48 -0500

    feat: new page InfoEducation

---
+++ src/pages/infoEducation/InfoEducation.jsx
@@ -0,0 +1,83 @@
+import { useEffect } from "react";
+import styles from "./InfoEducation.module.css";
+import { useParams } from "react-router-dom";
+import { AllCertifications } from "@/data/dataEducation";
+import { motion } from "framer-motion";
+import Tag from "@/components/tag/Tag";
+import MyFooter from "@/home/sections/myFooter/MyFooter";
+import LazyImage from "@/components/lazyImage/LazyImage";
+import ButtonBack from "@/components/buttonBack/ButtonBack";
+import HoverTag from "@/components/hoverTag/HoverTag";
+import ButtonPrimary from "@/components/buttonPrimary/ButtonPrimary";
+
+const InfoEducation = () => {
+  const { id } = useParams();
+  const education = AllCertifications.find((item) => item.url === id);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  if (!education) {
+    return <div>Educación no encontrada</div>;
+  }
+
+  return (
+    <motion.div
+      className={styles.detailEducation}
+      initial={{ opacity: "0", translateY: "100px" }}
+      animate={{ opacity: "1", translateY: "0" }}
+      exit={{ opacity: "0", translateY: "100px" }}
+      transition={{ duration: 0.3, ease: [0.215, 0.61, 0.355, 1] }}
+    >
+      <header className={styles.header}>
+        <span>
+          <ButtonBack />
+          <div className={styles.title}>
+            <h1>{education.title}</h1>
+          </div>
+        </span>
+        <div className={styles.cta}>
+          <ButtonPrimary
+            type="link"
+            text="Descargar certificado"
+            iconName="download"
+          />
+        </div>
+      </header>
+      <main className={styles.main}>
+        <section className={styles.certificate}>
+          <span>
+            <LazyImage
+              src={education.img}
+              alt={`Certificado de ${education.title}`}
+              style={{ width: "100%", height: "auto" }}
+              radius={20}
+            />
+          </span>
+        </section>
+        <section className={styles.detail}>
+          <div className={styles.about}>
+            <span>
+              <div className={styles.col}></div>
+              <div className={styles.col}>
+                <HoverTag label="Habilidades" />
+                <div className={styles.tags}>
+                  {education.skills.map((skill, index) => (
+                    <Tag tag={skill} index={index} key={index} />
+                  ))}
+                </div>
+              </div>
+            </span>
+            <div className={styles.col}>
+              <p>{education.description}</p>
+            </div>
+          </div>
+        </section>
+      </main>
+      <MyFooter />
+    </motion.div>
+  );
+};
+
+export default InfoEducation;
--- src/routing/AppRouter.jsx
+++ src/routing/AppRouter.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeView from "../home/HomeView";
-import InfoProjects from "../pages/infoProjects/InfoProjects";
+import InfoProjects from "@/pages/infoProjects/InfoProjects";
+import InfoEducation from "@/pages/infoEducation/InfoEducation";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeView />} />
-        <Route path="/education/:id" element={<q />} />
+        <Route path="/education/:id" element={<InfoEducation />} />
         <Route path="/project/:url" element={<InfoProjects />} />
       </Routes>
     </BrowserRouter>
