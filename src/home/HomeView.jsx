64a7e3e2afa56a945035587f7949cf69b06ca7ec
stivcode21 <stivenelcodificador@gmail.com>
2025-06-16 23:45:32 -0500

    chore: small change import

--- src/home/HomeView.jsx
+++ src/home/HomeView.jsx
@@ -1,13 +1,13 @@
 import styles from "./HomeView.module.css";
 import Navbar from "./sections/navbar/Navbar";
 import Header from "./sections/header/Header";
-import GitHubOverview from "@/components/gitHubOverview/GitHubOverview";
 import Projects from "./sections/projects/Projects";
 import About from "./sections/about/About";
 import Education from "./sections/education/Education";
 import MyFooter from "./sections/myFooter/MyFooter";
 import Contact from "./sections/contact/Contact";
 import { NotificationProvider } from "@/components/notificationProvider/notificationProvider";
+import GitHubOverview from "@/components/gitHubOverview/GitHubOverview";
 
 const HomeView = () => {
   return (
