d1a8a49bfc102733b818b8b7fda6d34b37d845b7
stivcode21 <stivenelcodificador@gmail.com>
2025-06-22 17:14:02 -0500

    feat: new component myStory create

---
+++ src/components/molecules/myStory/MyStory.jsx
@@ -0,0 +1,40 @@
+import styles from "./MyStory.module.css";
+
+const MyStory = () => {
+  return (
+    <>
+      <h1 className={styles.title}>MyStory</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        officia velit laudantium voluptatum saepe nulla repellat quo dolores
+        assumenda? Ipsam omnis tenetur recusandae vero veniam molestias
+        repellendus in iste. Vero? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Cupiditate officia velit laudantium voluptatum saepe
+        nulla repellat quo dolores assumenda? Ipsam omnis tenetur recusandae
+        vero veniam molestias repellendus in iste. Vero? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Cupiditate officia velit laudantium
+        voluptatum saepe nulla repellat quo dolores assumenda? Ipsam omnis
+        tenetur recusandae vero veniam molestias repellendus in iste. Vero?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        officia velit laudantium voluptatum saepe nulla repellat quo dolores
+        assumenda? Ipsam omnis tenetur recusandae vero veniam molestias
+        repellendus in iste. Vero? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Cupiditate officia velit laudantium voluptatum saepe
+        nulla repellat quo dolores assumenda? Ipsam omnis tenetur recusandae
+        vero veniam molestias repellendus in iste. Vero? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Cupiditate officia velit laudantium
+        voluptatum saepe nulla repellat quo dolores assumenda? Ipsam omnis
+        tenetur recusandae vero veniam molestias repellendus in iste. Vero?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        officia velit laudantium voluptatum saepe nulla repellat quo dolores
+        assumenda? Ipsam omnis tenetur recusandae vero veniam molestias
+        repellendus in iste. Vero? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Cupiditate officia velit laudantium voluptatum saepe
+        nulla repellat quo dolores assumenda? Ipsam omnis tenetur recusandae
+        vero veniam molestias repellendus in iste. Vero?
+      </p>
+    </>
+  );
+};
+
+export default MyStory;
