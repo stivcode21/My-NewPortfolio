ee6044edd62a946283e1efff472ca9c8a645bb0a
stivcode21 <stivenelcodificador@gmail.com>
2025-06-24 21:59:07 -0500

    chore: small  chage in myStory

--- src/components/molecules/myStory/MyStory.jsx
+++ src/components/molecules/myStory/MyStory.jsx
@@ -27,11 +27,7 @@ const MyStory = () => {
         tenetur recusandae vero veniam molestias repellendus in iste. Vero?
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
         officia velit laudantium voluptatum saepe nulla repellat quo dolores
-        assumenda? Ipsam omnis tenetur recusandae vero veniam molestias
-        repellendus in iste. Vero? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Cupiditate officia velit laudantium voluptatum saepe
-        nulla repellat quo dolores assumenda? Ipsam omnis tenetur recusandae
-        vero veniam molestias repellendus in iste. Vero?
+        assumenda? Ipsam omnis
       </p>
     </>
   );
