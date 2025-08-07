0546c577b3a522c8937e4c996d493d256aaee69a
stivcode21 <stivenelcodificador@gmail.com>
2025-08-07 09:01:58 -0500

    feat: new style tag

--- src/components/atoms/tag/Tag.jsx
+++ src/components/atoms/tag/Tag.jsx
@@ -1,8 +1,10 @@
 import styles from "./Tag.module.css";
 
 export default function Tag({ tag, index }) {
+  const main = tag === "MAIN-PROJECT" ? true : false;
+
   return (
-    <li key={index} className={styles.tag}>
+    <li key={index} className={`${styles.tag} ${main && styles.main}`}>
       {tag}
     </li>
   );
