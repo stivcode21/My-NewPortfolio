f8a46cae6cb0708e8ffc97148e2c17614648c81f
stivcode21 <stivenelcodificador@gmail.com>
2026-03-24 22:16:20 -0500

    feat: intregate new state for comments

--- src/store/ModalFormStore.js
+++ src/store/ModalFormStore.js
@@ -3,8 +3,10 @@ import { create } from "zustand";
 const useModalFormStore = create((set) => ({
   modalState: false,
   btnModalState: false,
+  commentDraft: "",
   setModalState: (state) => set({ modalState: state }),
   setBtnModalState: (state) => set({ btnModalState: state }),
+  setCommentDraft: (commentDraft) => set({ commentDraft }),
 }));
 
 export default useModalFormStore;
