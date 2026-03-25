7a8e3e9d7f56d5a5276244afcbd33a528849eb84
stivcode21 <stivenelcodificador@gmail.com>
2026-03-24 22:15:57 -0500

    feat: adapted this modal for use in formcomments

--- src/components/templates/modalOverlay/ModalOverlay.jsx
+++ src/components/templates/modalOverlay/ModalOverlay.jsx
@@ -7,8 +7,8 @@ const ModalOverlay = ({ children, isOpen, onClose }) => {
   return (
     <div className={styles.overlay} onClick={onClose}>
       <motion.div
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{
           duration: 0.5,
           delay: 0.2,
