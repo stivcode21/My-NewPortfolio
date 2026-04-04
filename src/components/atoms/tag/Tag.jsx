4426f9f286e059430c744216ce8eb9586ffba967
stivcode21 <stivenelcodificador@gmail.com>
2026-04-03 20:52:49 -0500

    update: now his layout is how old

--- src/components/atoms/tag/Tag.jsx
+++ src/components/atoms/tag/Tag.jsx
@@ -1,10 +1,8 @@
 import styles from "./Tag.module.css";
 
 export default function Tag({ tag, index }) {
-  const main = tag === "MAIN-PROJECT" ? true : false;
-
   return (
-    <li key={index} className={`${styles.tag} ${main && styles.main}`}>
+    <li key={index} className={styles.tag}>
       {tag}
     </li>
   );
