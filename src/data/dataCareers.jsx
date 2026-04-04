dc469c2cf8f6639eef7f1277e2836bd6fa3b51ff
stivcode21 <stivenelcodificador@gmail.com>
2026-04-03 20:56:37 -0500

    feat: Now the "about" section has updated information about me

--- src/data/dataCareers.jsx
+++ src/data/dataCareers.jsx
@@ -56,20 +56,26 @@ export const dataCareers2 = {
     ),
   },
   2: {
-    title: "UI/UX",
+    title: "Backend",
     svg: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
         height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
+        viewBox="0 0 48 48"
       >
-        <path d="M10 2v2" />
-        <path d="M14 2v4" />
-        <path d="M17 2a1 1 0 0 1 1 1v9H6V3a1 1 0 0 1 1-1z" />
-        <path d="M6 12a1 1 0 0 0-1 1v1a2 2 0 0 0 2 2h2a1 1 0 0 1 1 1v2.9a2 2 0 1 0 4 0V17a1 1 0 0 1 1-1h2a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1" />
+        <g fill="none">
+          <path
+            stroke="currentColor"
+            stroke-linejoin="round"
+            stroke-width="2.5"
+            d="M44 4H4v16h40zm0 24H4v16h40z"
+          />
+          <path
+            fill="currentColor"
+            d="M13 10h-2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1m0 24h-2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1m8-24h-2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1m0 24h-2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1"
+          />
+        </g>
       </svg>
     ),
   },
